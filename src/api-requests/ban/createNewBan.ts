9348f8d993df17e9aa444758ba8705bf136660f8
Ameil <ameilburke115@gmail.com>
2026-07-18 15:06:54 +1200

    cleanup

--- src/api-requests/ban/createNewBan.ts
+++ src/api-requests/ban/createNewBan.ts
@@ -1,37 +1,39 @@
 import type { AxiosError, AxiosResponse } from "axios";
 import axiosInstance from "@/utils/axiosInstance";
-import type { ApiRequestError, BanWithVenueBans, CreateBanDto } from "@/utils/interfaces";
+import type { CreateBanDto } from "@/utils/dtos";
+import type { ApiRequestError, Ban } from "@/utils/interfaces";
 import { isApiRequestError } from "@/utils/isApiRequestError";
 
-const createNewBan = async (createBanDto: CreateBanDto): Promise<BanWithVenueBans | AxiosError | ApiRequestError> => {
-    return await axiosInstance
-        .post("/bans", createBanDto)
-        .then((response: AxiosResponse) => {
+const createNewBan = async (
+	createBanDto: CreateBanDto,
+): Promise<Ban | AxiosError | ApiRequestError> => {
+	return await axiosInstance
+		.post("/bans", createBanDto)
+		.then((response: AxiosResponse) => {
+			if (response.data === null) {
+				return {
+					message: ["No data returned"],
+					error: "null_response",
+					statusCode: 500,
+				} as ApiRequestError;
+			}
+			return response.data;
+		})
+		.catch((error: AxiosError) => {
+			if (isApiRequestError(error.response?.data)) {
+				return error.response?.data as ApiRequestError;
+			}
 
-            if (response.data === null) {
-                return {
-                    message: ['No data returned'],
-                    error: 'null_response',
-                    statusCode: 500
-                } as ApiRequestError;
-            }
-            return response.data;
-        })
-        .catch((error: AxiosError) => {
-            if (isApiRequestError(error.response?.data)) {
-                return error.response?.data as ApiRequestError;
-            }
+			if (!error.response) {
+				return {
+					message: ["No data returned"],
+					error: "unknown",
+					statusCode: 500,
+				} as ApiRequestError;
+			}
 
-            if (!error.response) {
-                return {
-                    message: ['No data returned'],
-                    error: 'unknown',
-                    statusCode: 500
-                } as ApiRequestError;
-            }
-
-            return error;
-        });
+			return error;
+		});
 };
 
 export default createNewBan;
--- src/api-requests/banned-people/getAllBlanketBannedPeople.ts
+++ src/api-requests/banned-people/getAllBlanketBannedPeople.ts
@@ -1,13 +1,13 @@
 import type { AxiosError, AxiosResponse } from "axios";
 import axiosInstance from "@/utils/axiosInstance";
-import type { ApiRequestError, BannedPersonWithVenueBans } from "@/utils/interfaces";
+import type { ApiRequestError, BannedPerson } from "@/utils/interfaces";
 import { isApiRequestError } from "@/utils/isApiRequestError";
 
-const getAllBlanketBannedPeople = async (): Promise<BannedPersonWithVenueBans[] | AxiosError | ApiRequestError> => {
+const getAllBlanketBannedPeople = async (): Promise<BannedPerson[] | AxiosError | ApiRequestError> => {
 	return await axiosInstance
 		.get("/banned-people/blanket-banned")
 		.then((response: AxiosResponse) => {
-			return response.data as BannedPersonWithVenueBans[];
+			return response.data;
 		})
 		.catch((error: AxiosError) => {
 			if (isApiRequestError(error.response?.data)) {
